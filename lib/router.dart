b268dc5fc688be7b6bca25b40091cbd9b6300fa3
minhd <21020307@vnu.edu.vn>
2024-04-12 17:15:03 +0700

    adding firebase phone authentication

--- lib/router.dart
+++ lib/router.dart
@@ -1,15 +1,24 @@
 import 'package:chit_chat/common/widgets/error.dart';
 import 'package:chit_chat/features/auth/screens/login_screen.dart';
+import 'package:chit_chat/features/auth/screens/otp_screen.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
-  switch(settings.name) {
+  switch (settings.name) {
     case LoginScreen.routeName:
       return MaterialPageRoute(
         builder: (context) => const LoginScreen(),
       );
+    case LoginScreen.routeName:
+      final verificationId = settings.arguments as String;
+      return MaterialPageRoute(
+        builder: (context) => OTPScreen(
+          verificationId: verificationId,
+        ),
+      );
     default:
-      return MaterialPageRoute(builder: (context) => const Scaffold(
+      return MaterialPageRoute(
+        builder: (context) => const Scaffold(
           body: ErrorScreen(error: 'This page doesn`t exist'),
         ),
       );
