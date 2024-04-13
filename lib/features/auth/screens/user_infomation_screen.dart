5860210dcc3cd87af529f4a7e0120a6d4f6fbecb
HuyHoangty <hoang9555266@gmail.com>
2024-04-14 01:54:01 +0700

    User information screen UI

--- lib/features/auth/screens/user_infomation_screen.dart
+++ lib/features/auth/screens/user_infomation_screen.dart
@@ -1,11 +1,93 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:chit_chat/common/utils/utils.dart';
 
-class UserInformationScreen extends StatelessWidget {
+class UserInformationScreen extends ConsumerStatefulWidget {
   static const String routeName = '/user-information';
-  const UserInformationScreen({Key? key}) : super(key: key) ;
+  const UserInformationScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<UserInformationScreen> createState() =>
+      _UserInformationScreenState();
+}
+
+class _UserInformationScreenState extends ConsumerState<UserInformationScreen> {
+  final TextEditingController nameController = TextEditingController();
+  File? image;
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+  }
+
+  void selectImage() async {
+    image = await pickImageFromGallery(context);
+    setState(() {});
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  image == null
+                      ? const CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      'https://png.pngitem.com/pimgs/s/649-6490124_katie-notopoulos-katienotopoulos-i-write-about-tech-round.png',
+                    ),
+                    radius: 64,
+                  )
+                      : CircleAvatar(
+                    backgroundImage: FileImage(
+                      image!,
+                    ),
+                    radius: 64,
+                  ),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: selectImage,
+                      icon: const Icon(
+                        Icons.add_a_photo,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: size.width * 0.85,
+                    padding: const EdgeInsets.all(20),
+                    child: TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your name',
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: selectImage,
+                    icon: const Icon(
+                      Icons.done,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
