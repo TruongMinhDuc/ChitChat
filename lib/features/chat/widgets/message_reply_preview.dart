a9782fd5de84b0bce72ba6d3c307a17cb14a29a9
minhd <21020307@vnu.edu.vn>
2024-06-07 13:35:07 +0700

    modified reply feature

--- lib/features/chat/widgets/message_reply_preview.dart
+++ lib/features/chat/widgets/message_reply_preview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../colors.dart';
 import '../../../common/providers/message_reply_provider.dart';
 import 'display_media_file.dart';
 
@@ -16,43 +17,47 @@ class MessageReplyPreview extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final messageReply = ref.watch(messageReplyProvider);
 
-    return Container(
-      width: 350,
-      padding: const EdgeInsets.all(8),
-      decoration: const BoxDecoration(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(12),
-          topRight: Radius.circular(12),
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+        width: MediaQuery.sizeOf(context).width ,
+        padding: const EdgeInsets.all(8),
+        decoration: const BoxDecoration(
+          color: mobileChatBoxColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12),
+            topRight: Radius.circular(12),
+          ),
         ),
-      ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  messageReply!.isMe ? 'Me' : 'Opposite',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    messageReply!.isMe ? 'Me: ' : 'Opposite: ',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              GestureDetector(
-                child: const Icon(
-                  Icons.close,
-                  size: 16,
+                GestureDetector(
+                  child: const Icon(
+                    Icons.close,
+                    size: 16,
+                  ),
+                  onTap: () => cancelReply(ref),
                 ),
-                onTap: () => cancelReply(ref),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8),
-          DisplayTextImageGIF(
-            message: messageReply.message,
-            type: messageReply.messageEnum,
-          ),
-        ],
+              ],
+            ),
+            const SizedBox(height: 8),
+            DisplayTextImageGIF(
+              message: messageReply.message,
+              type: messageReply.messageEnum,
+            ),
+          ],
+        ),
       ),
     );
   }
