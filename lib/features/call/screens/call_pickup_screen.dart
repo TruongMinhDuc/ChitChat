c4dbdebd48207530229ef6045b521012275b3a1e
unknown <duongduchuyaz@gmail.com>
2024-06-11 10:44:15 +0700

    fix video call

--- lib/features/call/screens/call_pickup_screen.dart
+++ lib/features/call/screens/call_pickup_screen.dart
@@ -22,65 +22,67 @@ class CallPickupScreen extends ConsumerWidget {
           Call.fromMap(snapshot.data!.data() as Map<String, dynamic>);
 
           if (!call.hasDialled) {
-            return Scaffold(
-              body: Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Incoming Call',
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(height: 50),
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(call.callerPic),
-                      radius: 60,
-                    ),
-                    const SizedBox(height: 50),
-                    Text(
-                      call.callerName,
-                      style: const TextStyle(
-                        fontSize: 25,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                    const SizedBox(height: 75),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.call_end,
-                              color: Colors.redAccent),
+            return CallPickupScreen(
+              scaffold: Scaffold(
+                body: Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Incoming Call',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
                         ),
-                        const SizedBox(width: 25),
-                        IconButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => CallScreen(
-                                  channelId: call.callId,
-                                  call: call,
-                                  isGroupChat: false,
-                                ),
-                              ),
-                            );
-                          },
-                          icon: const Icon(
-                            Icons.call,
-                            color: Colors.green,
+                      ),
+                      const SizedBox(height: 50),
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(call.callerPic),
+                        radius: 60,
+                      ),
+                      const SizedBox(height: 50),
+                      Text(
+                        call.callerName,
+                        style: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      const SizedBox(height: 75),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.call_end,
+                                color: Colors.redAccent),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                          const SizedBox(width: 25),
+                          IconButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => CallScreen(
+                                    channelId: call.callId,
+                                    call: call,
+                                    isGroupChat: false,
+                                  ),
+                                ),
+                              );
+                            },
+                            icon: const Icon(
+                              Icons.call,
+                              color: Colors.green,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
